1c496b5d926727bf208977f132fe239c5666a21e
mohamed ali <vandetam97@gmail.com>
2023-04-10 05:46:24 +0200

    new commit

--- spicies/Foodies/settings.py
+++ spicies/Foodies/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = int(os.environ.get("DEBUG" , default=0))
-DEBUG=True
+DEBUG = int(os.environ.get("DEBUG" , default=0))
+
 ALLOWED_HOSTS =os.environ.get("ALLOWED_HOSTS").split(" ")
 #ALLOWED_HOSTS=["*"]
 
